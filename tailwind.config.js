beeae3e8786f78bb5682e7beb8cdd1d1024ec123
Eugene270987 <anikevich.it@gmail.com>
2024-03-16 19:42:13 +0200

    First commits to header

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main-color': '#000000',
+        'secondary-color': '#007b47',
+        'grey-color': '#3d3d3d',
+        'gold-color': '#b8860b',
+        'green-bg-color': '#057c48',
+        'main-bg-color': '#ffffff',
+        'grey-bg-color': '#C0C0C0',
+      }
+    },
+  },
+  plugins: [],
+}
+
